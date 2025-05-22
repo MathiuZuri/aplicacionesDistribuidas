1759dce04a9a83d92cde8b9ea527d7ce4073d5d4
NachyZuri <yukyuel@gmail.com>
2025-05-21 19:41:27 -0500

    update 10.3

--- ms-cliente/ms-cliente/obj/Debug/net9.0/ms-cliente.AssemblyInfo.cs
+++ ms-cliente/ms-cliente/obj/Debug/net9.0/ms-cliente.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ms-cliente")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+282898418c9ebb26b5b3a396ebd1c6d0e05898af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3b9b160cb415d2ac96e7cfc8cf07dbe93ee39dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ms-cliente")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ms-cliente")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
